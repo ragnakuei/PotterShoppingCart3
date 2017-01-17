8ac4c0e6ecbdccd75e65a64284a9594fe7aca75c
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-01-17 21:04:54 +0800

    第四個綠燈

--- ShoppingCartLib/ShoppingCartItem.cs
+++ ShoppingCartLib/ShoppingCartItem.cs
@@ -26,6 +26,9 @@ namespace ShoppingCartLib
                 case 3:
                     discount = 0.1m;
                     break;
+                case 4:
+                    discount = 0.2m;
+                    break;
             }
 
             return discount;
